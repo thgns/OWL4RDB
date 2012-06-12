e3219fdb01bf57aec8e9c5639bfcfd6dfd2c59a2
jwchoi <iamjwchoi@gmail.com>
2012-06-12 17:31:19 +0900

    created ImageURIs.java

---
+++ Janus/src/janus/ImageURIs.java
@@ -0,0 +1,34 @@
+package janus;
+
+public final class ImageURIs {
+	public static final String OK = "images/ok.png";
+	public static final String LOGO = "images/logo.png";
+	public static final String SPLASH = "images/splash.png";
+	
+	public static final String TOOLBAR_DUMP = "images/toolbar.dump.png";
+	public static final String TOOLBAR_GENERATE_TBOX = "images/toolbar.generate_tbox.png";
+	public static final String TOOLBAR_RUN = "images/toolbar.run.png";
+	public static final String TOOLBAR_SHOW_ATOMS = "images/toolbar.show_atoms.png";
+	
+	public static final String DB_CATALOG = "images/db.catalog.png";
+	public static final String DB_TABLE = "images/db.table.gif";
+	public static final String DB_FIELD = "images/db.field.png";
+	public static final String DB_KEY = "images/db.key.png";
+	
+	public static final String ONT_NAMED_CLS = "images/ont.named_class.png";
+	public static final String ONT_ANON_CLS = "images/ont.anonymous_class.png";
+	public static final String ONT_NAMED_EQUIVLNT_CLS = "images/ont.named_equivalent_class.png";
+	public static final String ONT_NAMED_OBJ_PROP = "images/ont.named_object_property.png";
+	public static final String ONT_ANON_OBJ_PROP = "images/ont.anonymous_object_property.png";
+	public static final String ONT_NAMED_DATA_PROP = "images/ont.named_data_property.png";
+	public static final String ONT_ANON_DATA_PROP = "images/ont.anonymous_data_property.png";
+	public static final String ONT_DATA_RANGE_BUT_DATA_TYPE = "images/ont.data_range_but_data_type.png";
+	public static final String ONT_KEYS = "images/ont.keys.png";
+	
+	public static final String DESCR_DATA_TYPE = "images/descr.data_type.png";
+	public static final String DESCR_DEFAULT_VALUE = "images/descr.default_value.png";
+	public static final String DESCR_LABLE = "images/descr.label.gif";
+	public static final String DESCR_CHECKED = "images/descr.checked.gif";
+	public static final String DESCR_UNCHECKED = "images/descr.unchecked.gif";
+	public static final String DESCR_NULL_VALUE = "images/descr.null.png";
+}
